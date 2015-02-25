1c587bedb93ec925d56cf8227e85d43ff0465892
krupet <krutsiuk.petr@gmail.com>
2015-02-25 17:27:41 +0200

    added first test

---
+++ user-manager/src/test/java/ua/com/joinit/BaseAppTest.java
@@ -0,0 +1,16 @@
+package ua.com.joinit;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import ua.com.joinit.config.UsersJavaAppConfig;
+
+/**
+ * Created by krupet on 25.02.2015.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = UsersJavaAppConfig.class) //TODO: add support for xml + javaConfig
+public abstract class BaseAppTest {
+}
---
+++ user-manager/src/test/java/ua/com/joinit/controller/ControllerTests.java
@@ -0,0 +1,35 @@
+package ua.com.joinit.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import ua.com.joinit.BaseAppTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+/**
+ * Created by krupet on 25.02.2015.
+ */
+public class ControllerTests extends BaseAppTest {
+    private MockMvc mockMvc;
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    protected WebApplicationContext wac;
+
+    @Before
+    public void setup() {
+        this.mockMvc = webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void simple() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+}
