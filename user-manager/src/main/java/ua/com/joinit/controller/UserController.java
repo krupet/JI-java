9908809431feca1973ebf110ea01a570891d3220
krupet <krutsiuk.petr@gmail.com>
2015-03-06 17:47:40 +0200

    gson added

--- user-manager/src/main/java/ua/com/joinit/controller/UserController.java
+++ user-manager/src/main/java/ua/com/joinit/controller/UserController.java
@@ -1,11 +1,16 @@
 package ua.com.joinit.controller;
 
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import ua.com.joinit.entity.User;
 import ua.com.joinit.service.UserService;
 
 /**
@@ -20,5 +25,13 @@ public class UserController {
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<String> getMessageFromUser(){
         return new ResponseEntity<>("hi", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public ResponseEntity<String> getUserById(@PathVariable("id") Long id) {
+        User user = new User("mock", "mock");
+        user.setId(id);
+        Gson gson = new Gson();
+        return  new ResponseEntity<>(gson.toJson(user), HttpStatus.OK);
     }
 }
--- user-manager/src/test/java/ua/com/joinit/controller/ControllerTests.java
+++ user-manager/src/test/java/ua/com/joinit/controller/ControllerTests.java
@@ -2,12 +2,17 @@ package ua.com.joinit.controller;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 import ua.com.joinit.BaseAppTest;
+import ua.com.joinit.service.UserService;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
@@ -22,14 +27,37 @@ public class ControllerTests extends BaseAppTest {
     @Autowired
     protected WebApplicationContext wac;
 
+//    @Autowired
+//    @Mock
+//    private UserService userService;
+//
+//    @Autowired
+//    @InjectMocks
+//    private UserController userController;
+
     @Before
     public void setup() {
         this.mockMvc = webAppContextSetup(this.wac).build();
+
     }
 
     @Test
     public void simple() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void get_user_by_id_and_expected_is_ok200() throws Exception {
+        mockMvc.perform(get("/1"))
+                .andExpect(status().is(200));
+    }
+
+    @Test
+    public void get_user_by_id_and_expected_is_valid_user() throws Exception {
+        String id = "12345";
+        mockMvc.perform(get("/" + id))
+                .andExpect(status().is(200))
+                .andDo(print());
     }
 }
